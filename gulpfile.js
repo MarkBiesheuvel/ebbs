4e68978b59137870d66539dcdc147f6e150b7417
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-01-14 16:10:36 +0100

    Write some es2016 code

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 
+gulp.task('js', () => {
+  gulp.src('src/main.js')
+    .pipe(babel({
+      presets: ['es2015', 'es2016']
+    }))
+    .pipe(gulp.dest('dist'))
+})
+
 gulp.task('default', () => {
-    gulp.src('src/main.js')
-      .pipe(babel({
-        presets: ['es2016']
-      }))
-      .pipe(gulp.dest('dist'))
-  }
-)
+  gulp.run('js')
+  gulp.watch('src/*.*', ['js'])
+})
--- src/main.js
+++ src/main.js
@@ -1,42 +1,40 @@
-var audioCtx = new AudioContext()
-var urlMap = {
+const audioCtx = new AudioContext()
+const urlMap = {
   'bd': 'Dirt-Samples/bd/BT3AADA.wav',
   'sn': 'Dirt-Samples/sn/ST3TAS3.wav'
 }
-var bufferMap = {}
+let bufferMap = {}
 
-var silence = function () {
-  return function () {
-
+const silence = () => {
+  return () => {
   }
 }
 
-var sample = function (key) {
+const sample = (key) => {
 
   if (!(key in urlMap)) {
-    console.warn('Invalid sample: ' + key)
+    console.warn(`Invalid sample: ${key}`)
     return silence;
   }
   if (!(key in bufferMap)) {
-    var url = urlMap[key]
 
-    var request = new XMLHttpRequest()
-    request.open('GET', url, true)
-    request.responseType = 'arraybuffer'
-    request.onload = function () {
-      audioCtx.decodeAudioData(request.response, function (buffer) {
-        bufferMap[key] = buffer
-      }, function (err) {
-        console.error(err)
+    fetch(urlMap[key])
+      .then((response) => {
+        return response.arrayBuffer()
       })
-    }
-    request.send()
+      .then((buffer) => {
+        audioCtx.decodeAudioData(buffer, (buffer) => {
+          bufferMap[key] = buffer
+        }, (err) => {
+          console.error(err)
+        })
+      })
   }
 
-  return function () {
+  return () => {
     // Still check if buffer is already loaded
     if (key in bufferMap) {
-      var source = audioCtx.createBufferSource()
+      let source = audioCtx.createBufferSource()
       source.buffer = bufferMap[key]
       source.connect(audioCtx.destination)
       source.start(0)
@@ -44,31 +42,28 @@ var sample = function (key) {
   }
 }
 
-var sequence = function () {
-
-  var callbacks = arguments
-  var n = callbacks.length
-
-  return function () {
-    for (var i = 0; i < n; i++) {
-      setTimeout(callbacks[i], i * 500)
-    }
+const sequence = (...callbacks) => {
+  return () => {
+    callbacks.forEach((callback, i) => {
+      setTimeout(callback, i * 500)
+    })
   }
 }
 
-var parallel = function () {
-
-  var callbacks = arguments
-  var n = callbacks.length
-
-  return function () {
-    for (var i = 0; i < n; i++) {
-      setTimeout(callbacks[i], i * 500)
-    }
+const parallel = (...callbacks) => {
+  return () => {
+    callbacks.forEach((callback) => {
+      callback()
+    })
   }
 }
 
-var loop = function (interval, callback) {
-  setInterval(callback, interval)
+const loop = (interval, callback) => {
+  const tmp = setInterval(callback, interval)
+
+  // Easy way to stop the loop
+  document.addEventListener("click", () => {
+    clearInterval(tmp)
+  });
 }
 
